67d6f930edd057640139b63ab47e492b3f4003c1
christophecraig <christophe.craig@viacesi.fr>
2017-06-08 12:32:01 +0200

    socket is now set to tell us how many people are connected, this will be to prevent 2 users from editing at the same time

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+var peopleConnected = 0
+
 app.set('port', (process.env.PORT || 8080))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket) {
+  io.emit('id')
+  peopleConnected ++
+  console.log('Il y a ' + peopleConnected + 'personnes connectées')
+  socket.on('disconnect', function() {
+    peopleConnected --
+    console.log('Il y a ' + peopleConnected + 'personnes connectées')
+  })
 })
 
 http.listen(8080, function() {
