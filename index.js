58ddc7438bcf2418c11ff7c3d84332aac91b0b9a
christophecraig <christophe.craig@viacesi.fr>
2017-06-08 11:51:45 +0200

    Now working when served with node

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var app = require('express')()
+var express = require('express')
+var app = express()
+var path = require('path')
 var http = require('http').createServer(app)
+app.set('port', (process.env.PORT || 8080))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
