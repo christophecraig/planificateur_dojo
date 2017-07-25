139dc9991773f7aaf0c984cb846a3d7235cc9de1
christophecraig <christophe.craig@viacesi.fr>
2017-07-25 11:37:14 +0200

    modified project structure

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// grab our gulp packages
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    watch = require('gulp-watch')
+csso = require('gulp-csso');
+
+// Watches every .scss file in public/sass/
+gulp.task('default', function () {
+    return gulp.watch('source/sass/**/*.scss', ['scss-compile'])
+})
+
+// Compiles scss files in public/sass/
+gulp.task('scss-compile', function () {
+            return gulp.src('source/sass/**/*.scss').pipe(sass()).pipe(csso({
+                    restructure: true,
+                    sourceMap: false,
+                    debug: true
+                }).pipe(gulp.dest('public/css')))
+            })
