2b462b82436a277c640f2b2386ae31ecff077de7
christophecraig <christophe.craig@viacesi.fr>
2017-08-21 17:32:40 +0200

    changed from canvas to svg -> would have been harder to track mouse events

--- public/js/project/components/tasks.js
+++ public/js/project/components/tasks.js
@@ -1,35 +1,36 @@
-define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueComponent'], function (declare, topic, lang, vueComponent) {
+define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueComponent', 'snapSVG', 'momentjs', 'frappe-gantt'], function (declare, topic, lang, vueComponent, Snap, moment, Gantt) {
     return declare(null, {
         constructor(compName) {
             this.compName = compName
             this.template = '#_task'
             this.data = {
-                sampleDev: {
-                    name: '',
-                    x: 30,
-                    y: 10,
-                    w: 85,
-                    h: 15
-                },
-                devs: {},
-                width: 110,
-                example: 'incroyable'
+                devs: {}
             }
-            this.computed = {
-                color() {
-                    return '#C03030'
+            this.methods = {
+                draw() {
+                    if (!document.getElementById('shape0')) {
+                        var svg = Snap(document.getElementById('svg'))
+
+                        // this.drawing = svg.circle(100, 100, 100)
+                        // this.drawing.attr({
+                        //     fill: 'blue',
+                        //     stroke: '#000'
+                        // })
+                        for (var i = 0; i < this.data.devs.length; i++) {
+                            this['dev' + i] = Snap(document.getElementById(['dev' + i]))
+                            this['shape' + i] = this['dev' + i].rect(20, 20 + 40 * i, 160, 35)
+                            this['shape' + i].attr({
+                                id: ['shape' + i],
+                            })
+                            document.getElementById(['shape' + i]).addEventListener('click', lang.hitch(this, 'whoami'))
+                        }
+                    }
                 },
-                startDate() {
-                    return this.$props.dev.plannedStartDate
+                whoami(e) {
+                    console.log('I am : ', e.target.id)
                 },
-                currentY() {
-                    return ''
-                },
-                height() {
-                    return ''
-                },
-                length() {
-                    return ''
+                move() {
+
                 }
             }
             this.directives = {
@@ -39,62 +40,65 @@ define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueCompo
                     var ctx = canvasEl.getContext("2d");
 
                     // Clear the canvas
+                    // for (var i = 0; i < _this.context.data.devs.length; i++) {
+                    // ctx.clearRect(15, 20, 300, 12)
+                    switch (_this.context.data.devs[i].id.fromProject) {
+                        case 'IIMPro':
+                            ctx.fillStyle = '#e03030'
+                            break
+                        case 'ReimsPro':
+                            ctx.fillStyle = 'green'
+                            break
+                        case 'CairnPro':
+                            ctx.fillStyle = 'blue'
+                            break
+                        case 'RAMEAUPro':
+                            ctx.fillStyle = 'orange'
+                            break
+                        case 'NumilogPro':
+                            ctx.fillStyle = 'purple'
+                            break
+                        case 'ModuleFRBRGestionPro':
+                            ctx.fillStyle = 'darkgrey'
+                            break
+                        case 'OptiPMBPro':
+                            ctx.fillStyle = 'darkblue'
+                            break
+                        case 'PHP7Pro':
+                            ctx.fillStyle = 'yellow'
+                            break
+                        case 'PMBKnowledgePro':
+                            ctx.fillStyle = '#e03030'
+                            break
+                        case 'CataAutoPro':
+                            ctx.fillStyle = '#e03030'
+                            break
+                        case 'RadioFrancePro':
+                            ctx.fillStyle = 'salmon'
+                            break
+                        case 'ModuleFRBRGestionPro':
+                            ctx.fillStyle = '#7f434f'
+                            break
+                        case 'RFDiversPro':
+                            ctx.fillStyle = '#47f2aa'
+                            break
+                        case 'ArteVODPro':
+                            ctx.fillStyle = '#47f2aa'
+                            break
+                        case 'Project_ID.1':
+                            ctx.fillStyle = '#47f2aa'
+                            break
+                        case 'trotroPro':
+                            ctx.fillStyle = 'red'
+                            break
+                    }
+                    ctx.fillRect(15, 20 + (40 * i), binding.value, 38)
+                    ctx.font = '18px Rubik, sans-serif'
+                    ctx.fillStyle = '#f2f2f2'
+                    ctx.fillText(_this.context.data.devs[i].id.fromProject, 20, 45 + (40 * i))
+                    // }
                     for (var i = 0; i < _this.context.data.devs.length; i++) {
-                        // ctx.clearRect(15, 20, 300, 12)
-                        switch (_this.context.data.devs[i].id.fromProject) {
-                            case 'IIMPro':
-                                ctx.fillStyle = '#e03030'
-                                break
-                            case 'ReimsPro':
-                                ctx.fillStyle = 'green'
-                                break
-                            case 'CairnPro':
-                                ctx.fillStyle = 'blue'
-                                break
-                            case 'RAMEAUPro':
-                                ctx.fillStyle = 'orange'
-                                break
-                            case 'NumilogPro':
-                                ctx.fillStyle = 'purple'
-                                break
-                            case 'ModuleFRBRGestionPro':
-                                ctx.fillStyle = 'darkgrey'
-                                break
-                            case 'OptiPMBPro':
-                                ctx.fillStyle = 'darkblue'
-                                break
-                            case 'PHP7Pro':
-                                ctx.fillStyle = 'yellow'
-                                break
-                            case 'PMBKnowledgePro':
-                                ctx.fillStyle = '#e03030'
-                                break
-                            case 'CataAutoPro':
-                                ctx.fillStyle = '#e03030'
-                                break
-                            case 'RadioFrancePro':
-                                ctx.fillStyle = 'salmon'
-                                break
-                            case 'ModuleFRBRGestionPro':
-                                ctx.fillStyle = '#7f434f'
-                                break
-                            case 'RFDiversPro':
-                                ctx.fillStyle = '#47f2aa'
-                                break
-                            case 'ArteVODPro':
-                                ctx.fillStyle = '#47f2aa'
-                                break
-                            case 'Project_ID.1':
-                                ctx.fillStyle = '#47f2aa'
-                                break
-                            case  'trotroPro':
-                                ctx.fillStyle = 'red'
-                                break
-                        }
-                        ctx.fillRect(15, 20 + (40 * i), binding.value, 38)
-                        ctx.font = '18px Rubik, sans-serif'
-                        ctx.fillStyle = '#f2f2f2'
-                        ctx.fillText(_this.context.data.devs[i].id.fromProject, 20, 55 + (40 * i))
+                        Snap('#calendar')
                     }
                     // Récupérer les dates + 
                     console.log('ok')
@@ -104,9 +108,19 @@ define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueCompo
             topic.subscribe('tasks', lang.hitch(this, 'storeDevs'))
             this.createComponent()
         },
+        
         storeDevs(devs) {
             this.data.devs = devs
             console.log(this)
+            var tasks = [{
+                id: 'Task 1',
+                name: 'Redesign website',
+                start: '2016-12-28',
+                end: '2016-12-31',
+                progress: 20,
+                dependencies: 'Task 2, Task 3'
+            }]
+            var __gantt = new Gantt("#gantt", tasks);
         },
         // drawTasks() {
         //     this.canvas = document.getElementById('canvas')
