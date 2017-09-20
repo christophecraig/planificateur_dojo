8b3463a7335dd4c2ab3155cd17e2fcaa57002454
christophecraig <christophe.craig@viacesi.fr>
2017-09-20 14:48:40 +0200

    remove comments and updated syntax to es6

--- public/js/project/components/menu.js
+++ public/js/project/components/menu.js
@@ -1,10 +1,9 @@
 define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueComponent'], function (declare, topic, lang, vueComponent) {
     return declare(null, {
-        constructor: function (compName) {
+        constructor(compName) {
             this.compName = compName
             this.template = '#menu_tpl'
             this.data = {
-                // TODO : pouvoir mettre les éléments du menu ici et plus dans le html
                 active: '',
                 lastView: '',
                 currentView: '',
@@ -31,7 +30,6 @@ define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueCompo
                     document.getElementById('burger').classList.toggle('is-open')
                 },
                 changeView(lastView, url) {
-                    console.log(url)
                     window.history.pushState(null, null, url)
                     this.data.currentView = window.location.pathname.slice(1)
                     this.data.lastView = lastView
@@ -44,15 +42,15 @@ define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueCompo
                 },
             }
             this.mounted = function () {
-                console.log()
+                
             }
             this.created = function () {
-                this.data.currentView = window.location.pathname.slice(1)                
+                this.data.currentView = window.location.pathname.slice(1)
             }
             this.createComponent()
         },
         createComponent() {
-            this.vue = new vueComponent(this.compName, this.template, this.data, this.methods, this.watch, this.mounted, this.computed, this.props, this.created, this.extended)
+            this.vue = new vueComponent(this.compName, this.template, this.data, this.methods, this.watch, this.mounted, this.computed, this.props,this.created, this.extended)
         }
     });
 });
