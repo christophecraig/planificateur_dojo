9dc00adb9e808d259fb3389a3741894126998915
christophecraig <christophe.craig@viacesi.fr>
2017-09-20 17:24:39 +0200

    removed notification title (useless)

--- public/js/project/components/notification.js
+++ public/js/project/components/notification.js
@@ -15,14 +15,19 @@ define(['dojo/_base/declare', 'dojo/topic', 'dojo/_base/lang', 'project/vueCompo
             topic.subscribe('notify', lang.hitch(this, 'showNotification'))
             this.createComponent()
         },
-        showNotification(type, title, message) {
-            this.data.tit = title
+        showNotification(type, message) {
+            document.getElementById('notification').classList = []
             this.data.typ = type
             this.data.message = message
+            if (type === 'success') {
+                document.getElementById('notification').classList.add('success')
+            } else if (type === 'failure') {
+                document.getElementById('notification').classList.add('failure')
+            }
             this.data.visible = true
             setTimeout(() => {
                 this.data.visible = false
-            }, 3000)
+            }, 4000)
         },
         createComponent() {
             this.vue = new vueComponent(this.compName, this.template, this.data, this.methods, this.watch, this.mounted, this.computed, this.props, this.created, this.updated, this.extended)
